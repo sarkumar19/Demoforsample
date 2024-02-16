0e65cb9f4ae740d4e8d9c60cba94a4dea1c2ae4f
sarkumar19 <155614234+sarkumar19@users.noreply.github.com>
2024-02-16 11:04:28 +0530

    Add files via upload
    uploaded the updated source file

---
+++ src/main/java/com/mypru/service/PruadviserService.java
@@ -0,0 +1,62 @@
+package com.mypru.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import com.mypru.entity.PruadviserEntity;
+import com.mypru.globalexceptionhandler.PruadviserNotFoundException;
+import com.mypru.pruadviserinterface.PruadviserInterface;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class PruadviserService {
+	Logger logger = LoggerFactory.getLogger(PruadviserService.class);
+
+	private final PruadviserInterface pruadviserinterface;
+	
+	
+    public PruadviserService(PruadviserInterface pruadviserinterface) {
+		this.pruadviserinterface = pruadviserinterface;
+	}
+     public List<PruadviserEntity> getAllDetails()
+     {
+    	 return pruadviserinterface.findAll();
+     }
+     
+     public PruadviserEntity getPruadviserDetailsByID(Long id)
+     { 
+    	 Optional<PruadviserEntity>  optionalpruadviser=pruadviserinterface.findById(id);
+    	 
+    	 return optionalpruadviser.orElseThrow(()->new PruadviserNotFoundException("The pruadviser details not found:" +id));
+    	 
+     
+    	 
+     }
+     public PruadviserEntity getPruadviserDetailsByName(String pruadviserName)
+     {
+    	 Optional<PruadviserEntity> optionalpru=pruadviserinterface.getByPruadviserName(pruadviserName);
+    	 return optionalpru.orElseThrow(()->new PruadviserNotFoundException("The given name is not found:"+pruadviserName));
+     }
+     
+     @Transactional
+     public PruadviserEntity saveAdviserDetails(PruadviserEntity pruadviser)
+     {
+      return pruadviserinterface.save(pruadviser);
+     
+      
+     }
+     
+     public void deleteAdviser()
+     {
+    	 pruadviserinterface.deleteAll();
+     }
+     @Transactional
+     public void deleteAdviserDetailsById(Long id)
+     {
+    	 pruadviserinterface.deleteAllById(id);
+     }
+     
+}
